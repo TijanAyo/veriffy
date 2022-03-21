bbdc54c96f0854fe2f841cc2cd613aab2db4308f
TijanAyo <ayomidetijani6@gmail.com>
2022-03-21 13:17:17 +0100

    view section

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,12 +3,26 @@ const router = express.Router()
 const {register, login } = require('../controllers/auth.controller')
 
 // @desc: Register Route
-// @route: POST /signup
+// @route: GET /account/register
+router.get('/account/register', (req, res)=>{
+    res.render('../views/index.ejs')
+})
+
+// @desc: Register Route
+// @route: POST /account/register
 router.post('/account/register', register)
 
 
+// @desc: Register Route
+// @route: GET /account/login
+router.get('/account/login', (req, res)=>{
+    res.render('../views/login.ejs')
+})
+
 // @desc: Login Route
-// @route: POST /signin
+// @route: POST /account/login
 router.post('/account/login', login)
+
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const ConnectDB = require('./config/db')
 require('dotenv').config()
 
 // middleware
+app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.use(express.static(__dirname + '/public'))
 
 
 app.get('/status', (req, res)=>{
